72780ea7a1dd6faeaf6bdef7828869b2d745712c
Ryan Bours <ryan.bours@gmail.com>
2023-10-31 17:22:53 +0100

    added nest/config + azuread bearer strat

--- services/user-service/src/app.module.ts
+++ services/user-service/src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AzureADStrategy } from './auth/strategies/azuread.strategy';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AzureADStrategy],
 })
 export class AppModule {}
